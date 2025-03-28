5ce1fbcafef7a2cc4c6e1bb95f8a5872efba9e2d
Gerdvila <egerdvila@gmail.com>
2025-03-28 03:41:38 +0200

    feat(CCS-10): calculator for homepage

--- src/main/java/com/leasing/calculator/api/controller/CalculatorController.java
+++ src/main/java/com/leasing/calculator/api/controller/CalculatorController.java
@@ -3,6 +3,12 @@ package com.leasing.calculator.api.controller;
 import com.leasing.calculator.api.model.request.MonthlyPaymentCalculationRequest;
 import com.leasing.calculator.api.model.response.MonthlyPaymentCalculationResponse;
 import com.leasing.calculator.service.MonthlyPaymentCalculatorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/public")
+@Tag(name = "Monthly Payment Calculator", description = "Provides functionality to calculate monthly payments for leasing services.")
 public class CalculatorController {
 
     private final MonthlyPaymentCalculatorService monthlyPaymentCalculatorService;
@@ -22,10 +29,22 @@ public class CalculatorController {
         this.monthlyPaymentCalculatorService = calculatorService;
     }
 
+    @Operation(
+            summary = "Calculate Monthly Payment",
+            description = "Calculates the monthly payment for leasing based on provided details such as car value, down payment, interest rate, and loan duration."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Monthly payment calculated successfully",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = MonthlyPaymentCalculationResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input provided",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
+    })
     @PostMapping("/calculator")
     @ResponseStatus(HttpStatus.OK)
     public MonthlyPaymentCalculationResponse calculateMonthlyPayment(@RequestBody MonthlyPaymentCalculationRequest monthlyPaymentCalculationRequest) {
         return monthlyPaymentCalculatorService.calculateMonthlyPayment(monthlyPaymentCalculationRequest);
     }
-
 }
