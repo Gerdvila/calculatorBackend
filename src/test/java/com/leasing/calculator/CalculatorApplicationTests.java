346d5e45f521571878c2616574905f1c9a5de89a
Gerdvila <egerdvila@gmail.com>
2025-05-22 01:36:56 +0300

    OBA(000): removed redundant code.

--- src/test/java/com/leasing/calculator/CalculatorApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.leasing.calculator;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CalculatorApplicationTests {
-}
